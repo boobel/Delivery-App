a6237e3d12e53e7cc3d02a00beb3527e6bcd7194
boobel <65288255+boobel@users.noreply.github.com>
2022-12-21 17:49:13 +0100

    added simple fetch functionality for meals from server

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
@@ -6,6 +6,22 @@ import { Footer } from "../components/Footer";
 const StyledWrapper = styled.div``;
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    /* //TODO JAKI ZWRACAĆ TYP W PROMISE */
+    const fetchMeals = async (): Promise<undefined> => {
+      try {
+        const data = await fetch("http://localhost:3000/meals");
+        const meals = await data.json();
+        console.log(meals);
+        return meals;
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    void fetchMeals();
+  }, []);
+
   return (
     <>
       <Header />
