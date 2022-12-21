ac1e8451c7aa0925a91f518df3cd43374ac9be3a
boobel <65288255+boobel@users.noreply.github.com>
2022-12-21 14:45:44 +0100

    boilerplate ready for components

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div``;
+
+const Home: React.FC = () => {
+  return <StyledWrapper></StyledWrapper>;
+};
+
+export { Home };
