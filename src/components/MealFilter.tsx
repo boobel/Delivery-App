a379cae747bf686d23442de81d188f53cabecba6
boobel <65288255+boobel@users.noreply.github.com>
2023-01-11 11:15:36 +0100

    created boilerplate file for menu item filters

---
+++ src/components/MealFilter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button``;
+
+const MealFilter: React.FC = () => {
+  return <StyledButton></StyledButton>;
+};
+
+export { MealFilter };
